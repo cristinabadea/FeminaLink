73288c35f9acbf9935223cbd500416d286cae3cb
KarenJParker77 <karen.mccrackle@gmail.com>
2022-03-02 21:29:28 +0000

    added JS to header on homepage and second page

--- SecondPage/secondPage.js
+++ SecondPage/secondPage.js
@@ -1,4 +1,29 @@
 /** @format */
+const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuBtn = document.querySelector(".mobile-menu-btn");
+
+mobileMenuBtn.addEventListener("click", () => {
+  mobileMenuBtn.classList.toggle("active-menu-btn");
+  mobileMenu.classList.toggle("slide-out");
+  mobileMenu.classList.toggle("slide-in");
+  if (mobileMenu.classList.contains("slide-out")) {
+    setTimeout(() => {
+      mobileMenu.classList.add("display-none");
+    }, 1000);
+  } else {
+    mobileMenu.classList.remove("display-none");
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 750) {
+    mobileMenu.classList.add("slide-out");
+    mobileMenu.classList.remove("slide-in");
+    setTimeout(() => {
+      mobileMenu.classList.add("display-none");
+    }, 1000);
+  }
+});
 
 let container = document.getElementsByClassName("container-fluid")[0];
 console.log(container);
